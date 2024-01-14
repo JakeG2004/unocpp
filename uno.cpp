29058f0786c3457b5d991dcc5deca4ca15749db2
jake <jake.lgendreau@gmail.com>
2024-01-13 21:06:59 -0800

    Distributes cards to all agents

--- uno.cpp
+++ uno.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 
-enum cards {
+using namespace std;
+
+enum vars {
+    PLAYER = 0,
+    BOT = 1,
     SKIP = 10,
     REVERSE = 11,
     PLUS2 = 12,
@@ -9,35 +13,82 @@ enum cards {
     WILD4 = 14
 };
 
-using namespace std;
-
 struct card{
     int color;
     int num;
 };
 
-struct player{
+struct agent{
     vector<card> hand;
+    int type;
 };
 
 vector<card> makeDeck();
+vector<agent> makeAgents(int, int);
+void distributeCards(vector<card>& deck, vector<agent>& agents);
 
 int main(int argc, char* argv[]){
 
     //Ensure correct usage
-    if(argc != 2){
-        cout << "Incorrect usage! Proper usage: ./uno.out <number of players>" << endl;
+    /*if(argc != 3){
+        cout << "Incorrect usage! Proper usage: ./uno.out <number of players> <number of bots>" << endl;
         return -1;
-    }
-
-    int numPlayers = stoi(argv[1]);
-
-    vector<card> deck = makeDeck();
-
-    /*for(int i=0; i<deck.size(); i++){
-        cout << deck[i].color << "\t" << deck[i].num << endl;
     }*/
 
+    //int numPlayers = stoi(argv[1]);
+    //int numBots = stoi(argv[2]);
+
+    int numPlayers = 2;
+    int numBots = 1;
+
+    if(numPlayers + numBots > 15){
+        cout << "Error: Too many agents to play the game. Max of 15." << endl;
+        return -2;
+    }
+
+    //add players and bots to agent vector
+    vector<agent> agents = makeAgents(numPlayers, numBots);
+
+    //Establish deck
+    vector<card> deck = makeDeck();
+
+    //Distribute cards
+    distributeCards(deck, agents);
+
+    for(int i=0; i<7; i++){
+        cout << agents[0].hand[i].num << endl;
+    }
+}
+
+void distributeCards(vector<card>& deck, vector<agent>& agents){
+    //deal 7 cards to every agent
+    for(int i=0; i<agents.size(); i++){
+        while(agents[i].hand.size() < 7){
+            int tmp = rand() % deck.size(); //choose random card from deck
+            agents[i].hand.push_back(deck[tmp]); //put card into agent's hand
+            deck.erase(deck.begin() + tmp); //erase card from deck
+        }
+    }
+}
+
+vector<agent> makeAgents(int numPlayers, int numBots){
+
+    vector<agent> agents;
+
+    for(int i=0; i<numPlayers + numBots; i++){
+
+        //add players
+        agent tmp;
+        tmp.type = PLAYER;
+
+        //add bots
+        if(i >= numPlayers)
+            tmp.type = BOT;
+
+        agents.push_back(tmp);
+    }
+
+    return agents;
 }
 
 vector<card> makeDeck(){
@@ -54,7 +105,7 @@ vector<card> makeDeck(){
             tmp.num = j;
 
             //append it to deck
-            deck.insert(deck.end(), tmp);
+            deck.push_back(tmp);
         }
     }
 
@@ -68,7 +119,7 @@ vector<card> makeDeck(){
             tmp.num = j;
 
             //append it to deck
-            deck.insert(deck.end(), tmp);
+            deck.push_back(tmp);
         }
     }
     
